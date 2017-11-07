9507b832d478c4beaf4bb36602df8f8dfcba6f2d
Osniel Teixeira <niellt@hotmail.com>
2017-11-07 16:06:52 -0200

    Criação de struct para circuito e classe codable para as coordenadas

---
+++ MiniChallenge5/Circuito.swift
@@ -0,0 +1,9 @@
+//
+//  Circuito.swift
+//  MiniChallenge5
+//
+//  Created by Osniel Lopes Teixeira on 06/11/2017.
+//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
---
+++ MiniChallenge5/Coordinate.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinate.swift
+//  MiniChallenge5
+//
+//  Created by Osniel Lopes Teixeira on 07/11/2017.
+//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
