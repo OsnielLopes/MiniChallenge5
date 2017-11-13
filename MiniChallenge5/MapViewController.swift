9b73b6cfcbc5036d1a22212616c96251cc50a8e8
Osniel Teixeira <niellt@hotmail.com>
2017-11-12 22:47:34 -0200

    Implementa a tela de mapa, que já permite inserção de novos pins e leitura direto do servidor

--- MiniChallenge5/MapViewController.swift
+++ MiniChallenge5/MapViewController.swift
@@ -40,48 +40,44 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getCircuitFromServer()
+    }
+    
     @IBAction func didTouchSave(_ sender: Any) {
         
         if lastValidCoordinate != nil{
-            circuit.coordinates.append(lastValidCoordinate)
-        }
-        
-        let message:String = "Sua localização atual é "+String(describing: lastValidCoordinate)
-        let alert = UIAlertController(title: "Alerta", message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-        
-        let jsonEncoder = JSONEncoder()
-        do {
-            let jsonData = try jsonEncoder.encode(circuit)
-            
-            let url = URL(string: "https://exemplo1nodejs.herokuapp.com/circuito")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            
-            let task = URLSession.shared.uploadTask(with: request, from: jsonData) { data, response, error in
-                if let error = error {
-                    // handle the transport error
-                    print(error.localizedDescription)
-                    return
+            let jsonEncoder = JSONEncoder()
+            do {
+                let jsonData = try jsonEncoder.encode(circuit)
+                
+                let url = URL(string: "https://exemplo1nodejs.herokuapp.com/circuito")!
+                var request = URLRequest(url: url)
+                request.httpMethod = "POST"
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                let task = URLSession.shared.uploadTask(with: request, from: jsonData) { data, response, error in
+                    if let error = error {
+                        // handle the transport error
+                        print(error.localizedDescription)
+                        return
+                    }
                 }
-                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                    // handle the server error
-                    return
-                }
-//                if response.mimeType == "text/plain" || response.mimeType == "text/plain",
-//                                    let data = data {
-//                                    // use the data
-//                                }
+                task.resume()
+                circuit.coordinates.append(lastValidCoordinate)
+                updatePins()
+            } catch {
+                print("Impossible to generate JSON from circuit")
             }
-            task.resume()
-            
-        } catch {
-            print("Impossible to generate JSON from circuit")
         }
-
+        
     }
+    
+    // MARK: - Map
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         lastValidCoordinate = locations.last?.toCoordinate()
@@ -99,6 +95,36 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         } else {
             locationManager.requestWhenInUseAuthorization()
         }
+    }
+    
+    func updatePins() {
+        for l in circuit.coordinates{
+            mapView.addAnnotation(MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: l.latitude, longitude: l.longitude)))
+        }
+    }
+    
+    //MARK - Funcs
+    func getCircuitFromServer(){
+        let url = URL(string: "https://exemplo1nodejs.herokuapp.com/circuito")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let data = data{
+                let decoder = JSONDecoder()
+                DispatchQueue.main.async {
+                    do {
+                        self.circuit = try decoder.decode(Circuit.self, from: data)
+                        self.updatePins()
+                    } catch {
+                        print("Impossible to decode to circuit from data")
+                    }
+                }
+            }
+        }
+        task.resume()
     }
     
     /*
