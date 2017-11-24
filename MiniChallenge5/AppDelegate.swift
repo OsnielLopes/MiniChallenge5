13e4953146becef9d5f23e72517ffd1a70bb836c
Vinicius Cano <viniciuscano.vc@gmail.com>
2017-11-24 15:12:56 -0200

    Retirando status bar

--- MiniChallenge5/AppDelegate.swift
+++ MiniChallenge5/AppDelegate.swift
@@ -12,10 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        UIApplication.shared.statusBarStyle = .lightContent
-        
+    
+    var preferesStatusBarHidden: Bool {
         return true
     }
 
