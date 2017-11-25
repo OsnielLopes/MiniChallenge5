7e9314ad8cb66fc31d46c9237b47d6bdcf4e5ca1
= <renan.rsg@hotmail.com>
2017-11-25 17:46:10 -0200

    Criação do modelo Player

---
+++ MiniChallenge5/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  MiniChallenge5
+//
+//  Created by Renan Soares Germano on 25/11/17.
+//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
+
+struct Player: Codable{
+    var id: Int
+    var name: String
+    var email: String
+    var password: String
+    var faction: Faction
+}
