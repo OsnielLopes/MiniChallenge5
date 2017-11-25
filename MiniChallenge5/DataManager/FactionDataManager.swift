c08677f48b016fd09d692a516811d168e2871e90
= <renan.rsg@hotmail.com>
2017-11-25 17:39:28 -0200

    Criação do Modelo Faction

--- MiniChallenge5/DataManager/FactionDataManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FactionDataManager.swift
-//  MiniChallenge5
-//
-//  Created by Renan Soares Germano on 24/11/17.
-//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
-//
-
-import Foundation
--- MiniChallenge5/Model/Faction.swift
+++ MiniChallenge5/Model/Faction.swift
@@ -2,13 +2,13 @@
 //  Faction.swift
 //  MiniChallenge5
 //
-//  Created by Renan Soares Germano on 24/11/17.
+//  Created by Renan Soares Germano on 25/11/17.
 //  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
 //
 
 import Foundation
 
 struct Faction: Codable{
-    var id: Int!
-    var name: String!
+    var id: Int?
+    var name: String
 }
