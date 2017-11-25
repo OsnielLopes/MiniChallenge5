f199103c6df27be3074778d99c4121aab53016c0
= <renan.rsg@hotmail.com>
2017-11-25 17:52:18 -0200

    Criação da Classe PlayerDataManager e assinatura de suas funções

---
+++ MiniChallenge5/DataManager/PlayerDataManager.swift
@@ -0,0 +1,37 @@
+//
+//  PlayerDataManager.swift
+//  MiniChallenge5
+//
+//  Created by Renan Soares Germano on 25/11/17.
+//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
+
+class PlayerDataManager{
+    
+    //MARK: Create Player
+    func create(player: Player, callback: @escaping (_ : Player) -> Void){
+        
+    }
+    
+    //MARK: Read all Players
+    func read(callback: @escaping (_ : [Player]) -> Void){
+        
+    }
+    
+    //MARK: Read Player by id
+    func readById(id: Int, callback: @escaping (_ : Player) -> Void){
+        
+    }
+    
+    //MARK: Update Player
+    func update(player: Player, callback: @escaping (_ : Player) -> Void){
+        
+    }
+    
+    //MARK: Delete Player
+    func delete (id: Int, callback: @escaping (_ : Player) -> Void){
+        
+    }
+}
