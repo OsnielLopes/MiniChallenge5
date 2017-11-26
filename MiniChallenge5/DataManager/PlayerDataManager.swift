5a039cfdf7a0922a66a86b69ec266b58cfaf70e1
= <renan.rsg@hotmail.com>
2017-11-26 00:16:10 -0200

    Implementação do método Create da classe PlayerDataManager

--- MiniChallenge5/DataManager/PlayerDataManager.swift
+++ MiniChallenge5/DataManager/PlayerDataManager.swift
@@ -12,7 +12,37 @@ class PlayerDataManager{
     
     //MARK: Create Player
     func create(player: Player, callback: @escaping (_ : Player) -> Void){
-        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(player)
+            
+            print("Encoded Data: \(String(data: jsonData, encoding: .utf8))")
+            let url = URL(string: "https://cyber-runner.herokuapp.com/player")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let task = URLSession.shared.uploadTask(with: request, from: jsonData) { data, response, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                let httpResponse = response as! HTTPURLResponse
+                print("Request Status Code: \(httpResponse.statusCode)")
+                if let data = data{
+                    let decoder = JSONDecoder()
+                    do {
+                        let player: Player = try decoder.decode(Player.self, from: data)
+                        callback(player)
+                    } catch {
+                        print("Impossible to decode to player from data")
+                    }
+                }
+            }
+            task.resume()
+        } catch {
+            print("Impossible to generate JSON from circuit")
+        }
     }
     
     //MARK: Read all Players
