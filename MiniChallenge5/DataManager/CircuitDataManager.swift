d655b7236c4d30cd16881f45950a066134915602
= <renan.rsg@hotmail.com>
2017-11-29 19:41:21 -0200

    Término da implementação de todas as funções da classe CircuitDataManager

--- MiniChallenge5/DataManager/CircuitDataManager.swift
+++ MiniChallenge5/DataManager/CircuitDataManager.swift
@@ -10,7 +10,13 @@ import Foundation
 
 class CircuitDataManager : CircuitDataManagerProtocol{
     func getCircuitLocations(callback: @escaping ([Bow]) -> ()) {
-        //TODO: implementar esse método
+        var firstBows: [Bow] = [Bow]()
+        self.read {
+            $0.forEach{
+                firstBows.append($0.bows.first!)
+            }
+            callback(firstBows)
+        }
     }
     
     //MARK: Create Circuit
@@ -18,7 +24,6 @@ class CircuitDataManager : CircuitDataManagerProtocol{
         let jsonEncoder = JSONEncoder()
         do {
             let jsonData = try jsonEncoder.encode(circuit)
-            print("Encoded Data: \(String(data: jsonData, encoding: .utf8))")
             let url = URL(string: "https://cyber-runner.herokuapp.com/circuit")!
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
@@ -29,19 +34,10 @@ class CircuitDataManager : CircuitDataManagerProtocol{
                     print(error.localizedDescription)
                     return
                 }
-                
-                let httpResponse = response as! HTTPURLResponse
-                print("Request Status Code: \(httpResponse.statusCode)")
-                
                 if let data = data{
-                    var recievedData: String = String(data: data, encoding: .utf8)!
-                    recievedData = recievedData.replacingOccurrences(of: ":[{\"latitude\":\"", with: ":[{\"latitude\":")
-                                               .replacingOccurrences(of: "\"},{\"latitude\":\"", with: "},{\"latitude\":")
-                                               .replacingOccurrences(of: "\",\"longitude\":\"", with: ",\"longitude\":")
-                                               .replacingOccurrences(of: "\"}],\"id\":", with: "}],\"id\":")
                     let decoder = JSONDecoder()
                     do {
-                        let circuit: Circuit = try decoder.decode(Circuit.self, from: recievedData.data(using: .utf8)!)
+                        let circuit: Circuit = try decoder.decode(Circuit.self, from: self.clean(data: data))
                         callback(circuit)
                     } catch {
                         print("Impossible to decode to circuit from data")
@@ -56,12 +52,46 @@ class CircuitDataManager : CircuitDataManagerProtocol{
     
     //MARK: Read all Circuits
     func read(callback: @escaping (_ : [Circuit]) -> Void){
+        let url = URL(string: "https://cyber-runner.herokuapp.com/circuit")!
         
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let data = data{
+                let decoder = JSONDecoder()
+                do {
+                    let circuits: [Circuit] = try decoder.decode([Circuit].self, from: self.clean(data: data))
+                    callback(circuits)
+                } catch {
+                    print("Impossible to decode to Circuit from data")
+                }
+            }
+        }
+        task.resume()
     }
     
     //MARK: Read Circuit by id
     func readById(id: Int, callback: @escaping (_ : Circuit) -> Void){
+        let url = URL(string: "https://cyber-runner.herokuapp.com/circuit/\(id)")!
         
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let data = data{
+                let decoder = JSONDecoder()
+                do {
+                    let circuit: Circuit = try decoder.decode(Circuit.self, from: self.clean(data: data))
+                    callback(circuit)
+                } catch {
+                    print("Impossible to decode to Faction from data")
+                }
+            }
+        }
+        task.resume()
     }
     
     //MARK: Update Circuit
@@ -71,6 +101,41 @@ class CircuitDataManager : CircuitDataManagerProtocol{
     
     //MARK: Delete Circuit
     func delete (id: Int, callback: @escaping (_ : Circuit) -> Void){
-        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let url = URL(string: "https://cyber-runner.herokuapp.com/circuit/\(id)")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "DELETE"
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                if let data = data{
+                    let decoder = JSONDecoder()
+                    do {
+                        let circuit: Circuit = try decoder.decode(Circuit.self, from: self.clean(data: data))
+                        callback(circuit)
+                    } catch {
+                        print("Impossible to decode to Faction from data")
+                    }
+                }
+            }
+            task.resume()
+        } catch {
+            print("Impossible to generate JSON from circuit")
+        }
+    }
+    
+    //MAKR: Aux funcs
+    private func clean(data: Data) -> Data{
+        var recievedData: String = String(data: data, encoding: .utf8)!
+        recievedData = recievedData.replacingOccurrences(of: ":[{\"latitude\":\"", with: ":[{\"latitude\":")
+            .replacingOccurrences(of: "\"},{\"latitude\":\"", with: "},{\"latitude\":")
+            .replacingOccurrences(of: "\",\"longitude\":\"", with: ",\"longitude\":")
+            .replacingOccurrences(of: "\"}],\"id\":", with: "}],\"id\":")
+            .replacingOccurrences(of: "\"}]}", with: "}]}")
+        return recievedData.data(using: .utf8)!
     }
 }
