9bf60b060b33dc458f724bf54ef9921f82ca2833
= <renan.rsg@hotmail.com>
2017-12-02 16:25:44 -0200

    Ajuste das classe de Model

--- MiniChallenge5/Model/Faction.swift
+++ MiniChallenge5/Model/Faction.swift
@@ -12,4 +12,9 @@ struct Faction: Codable{
     //MARK: Properties
     var id: Int?
     var name: String
+    
+    init(id: Int?, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
--- MiniChallenge5/Model/Play.swift
+++ MiniChallenge5/Model/Play.swift
@@ -8,8 +8,48 @@
 
 import Foundation
 
-struct Move: Codable{
+struct Play: Codable{
+    //MARK: Properties
+    var id: Int?
+    var circuitID: Int
     var player: Player
-    var circuit: Circuit
-    var time: Int
+    var seconds: Int
+    var milliseconds: Int
+    
+    //MARK: Types
+    enum CodingKeys: String, CodingKey {
+        case id
+        case circuitID = "circuit_id"
+        case playerID = "player_id"
+        case player
+        case seconds
+        case milliseconds
+    }
+    
+    //MARK: Initializers
+    init(id: Int?, circuitID: Int, player: Player, seconds: Int, milliseconds: Int) {
+        self.id = id
+        self.circuitID = circuitID
+        self.player = player
+        self.seconds = seconds
+        self.milliseconds = milliseconds
+    }
+    
+    //MARK: Encode and Decode functions
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try values.decode(Int.self, forKey: .id)
+        self.circuitID = try values.decode(Int.self, forKey: .circuitID)
+        self.player = try values.decode(Player.self, forKey: .player)
+        self.seconds = try values.decode(Int.self, forKey: .seconds)
+        self.milliseconds = try values.decode(Int.self, forKey: .milliseconds)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.circuitID, forKey: .circuitID)
+        try container.encode(self.player.id, forKey: .playerID)
+        try container.encode(self.seconds, forKey: .seconds)
+        try container.encode(self.milliseconds, forKey: .milliseconds)
+    }
 }
