9cc17df231f3edaa0b0c76b9dae227257285f871
Osniel Teixeira <niellt@hotmail.com>
2017-12-05 16:51:45 -0200

    Alterações na classe ChooseGameModeViewController

--- MiniChallenge5/ViewControllers/ChooseGameModeViewController.swift
+++ MiniChallenge5/ViewControllers/ChooseGameModeViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 import MapKit
 
 class ChooseGameModeViewController: UIViewController {
-
+    
     @IBOutlet weak var playTheLocalCircuitButton: UIButton!
     let locationManager:CLLocationManager = CLLocationManager()
     let circuitManager:CircuitDataManager = CircuitDataManager()
+    var closerCircuitId:Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        playTheLocalCircuitButton.isEnabled = closerCircuitId != nil
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -28,30 +31,32 @@ class ChooseGameModeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         checkLocationAuthorizationStatus()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        //distanceFilter for the locationManager is not defined, keeping the default value
         locationManager.startUpdatingLocation()
         circuitManager.getCircuitLocations { (circuits) in
             for (id, location) in circuits{
                 let newLocation = CLLocation(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude))
-
+                if newLocation.distance(from: self.locationManager.location!) < 200 {
+                    self.closerCircuitId = id
+                    break
+                }
             }
         }
     }
     
-
+    
     func checkLocationAuthorizationStatus() {
         if !(CLLocationManager.authorizationStatus() == .authorizedWhenInUse) {
             locationManager.requestWhenInUseAuthorization()
         }
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
