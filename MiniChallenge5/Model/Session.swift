5c1cd099c2559361dbaecff8f729620ab6112193
= <renan.rsg@hotmail.com>
2017-12-05 16:29:13 -0200

    Ajuste da classe de Player

---
+++ MiniChallenge5/Model/Session.swift
@@ -0,0 +1,9 @@
+//
+//  Session.swift
+//  MiniChallenge5
+//
+//  Created by Renan Soares Germano on 05/12/17.
+//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
