ab10e78922673b907a2e30e85330874a26d0820a
= <renan.rsg@hotmail.com>
2017-12-06 19:06:28 -0200

    Inserção dos fins de circuito no mapa

--- MiniChallenge5/ViewControllers/MapViewController.swift
+++ MiniChallenge5/ViewControllers/MapViewController.swift
@@ -16,50 +16,40 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     //MARK: Properties
-    let locationManager:CLLocationManager = CLLocationManager()
-    var lastValidBow: Bow!
-    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    let locationManager: CLLocationManager = CLLocationManager()
     let regionRadius: CLLocationDistance = 1000
-    var circuit: Circuit = Circuit.init()
+    let locations: [Bow] = [Bow]()
+    let circuitDataManager: CircuitDataManager = CircuitDataManager()
+    
     
     //MARK: Life cicle functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Configuring the map
-        centerMapOnLocation(location: initialLocation)
-        
-        //Configuring the locationManager
-        checkLocationAuthorizationStatus()
         locationManager.delegate = self
+        checkLocationAuthorizationStatus()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        //distanceFilter for the locationManager is not defined, keeping the default value
-        locationManager.startUpdatingLocation()
+        centerMapOnLocation(location: locationManager.location!)
         
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        self.circuitDataManager.getCircuitLocations(callback: {locations in
+            for (_, location) in locations {
+                self.mapView.addAnnotation(
+                    MKPlacemark(
+                        coordinate: CLLocationCoordinate2D(
+                            latitude: location.latitude,
+                            longitude: location.longitude)
+                    )
+                )
+            }
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.tintColor = UIColor.white
-        self.navigationController?.navigationBar.backgroundColor = UIColor.init(red: 89.0/255.00, green: 46.0/255.0, blue: 185.0/255.0, alpha: 0.57)
-        getCircuitFromServer()
+        self.navigationController?.navigationBar.backgroundColor =
+            UIColor(red: 89.0/255.00, green: 46.0/255.0, blue: 185.0/255.0, alpha: 0.57)
     }
     
-    // MARK: - Map
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        lastValidBow = locations.last?.toCoordinate()
-    }
-    
+    //MARK: Aux functions
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
@@ -75,43 +65,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func updatePins() {
-        for l in circuit.bows{
-            mapView.addAnnotation(MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: l.latitude, longitude: l.longitude)))
-        }
+//        for l in circuit.bows{
+//            mapView.addAnnotation(MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: l.latitude, longitude: l.longitude)))
+//        }
     }
-    
-    //MARK - Funcs
-    func getCircuitFromServer(){
-        let url = URL(string: "https://exemplo1nodejs.herokuapp.com/circuito")!
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            if let data = data{
-                let decoder = JSONDecoder()
-                DispatchQueue.main.async {
-                    do {
-                        self.circuit = try decoder.decode(Circuit.self, from: data)
-                        self.updatePins()
-                    } catch {
-                        print("Impossible to decode to circuit from data")
-                    }
-                }
-            }
-        }
-        task.resume()
-    }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
