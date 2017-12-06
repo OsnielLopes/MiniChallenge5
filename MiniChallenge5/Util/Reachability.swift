2389f40d13d0c73b1985d352ed9a05eed8a87708
= <renan.rsg@hotmail.com>
2017-12-06 15:12:36 -0200

    Movendo arquivo PreferencesDataManager para o group Util

---
+++ MiniChallenge5/Util/Reachability.swift
@@ -0,0 +1,9 @@
+//
+//  Reachability.swift
+//  MiniChallenge5
+//
+//  Created by Renan Soares Germano on 06/12/17.
+//  Copyright © 2017 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import Foundation
