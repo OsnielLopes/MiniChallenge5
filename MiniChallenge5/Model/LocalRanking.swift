17ed08bdedd443c2e1f84909e8956f0c105830ff
Osniel Teixeira <niellt@hotmail.com>
2017-12-08 13:41:46 -0200

    Ultimo push

--- MiniChallenge5/Model/LocalRanking.swift
+++ MiniChallenge5/Model/LocalRanking.swift
@@ -14,21 +14,9 @@ class LocalRanking: Ranking {
     private static var plays = [Play]()
     
     static func add(_ play: Play){
-        if plays.count == 0{
-            plays.append(play)
-        } else{
-            for i in stride(from: plays.count-1, to: -1, by: -1){
-                if plays[i].seconds <= play.seconds {
-                    if plays[i].milliseconds < play.milliseconds {
-                        plays.insert(play, at: i+1)
-                    } else {
-                        plays.insert(play, at: i)
-                    }
-                    break
-                } else if i == 0 {
-                    plays.insert(play, at: 0)
-                }
-            }
+        plays.append(play)
+        plays = plays.sorted { (a, b) -> Bool in
+            return a.milliseconds < b.milliseconds && a.seconds < b.seconds
         }
         
     }
