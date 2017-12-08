ce991825cd0dbadcb0c013bcec71d144559c29e2
Osniel Teixeira <niellt@hotmail.com>
2017-12-08 02:29:09 -0200

    Implementação da classe SearchViewController para ajudar o usuário a localizar um circuito

--- MiniChallenge5/ViewControllers/SearchViewController.swift
+++ MiniChallenge5/ViewControllers/SearchViewController.swift
@@ -7,20 +7,68 @@
 //
 
 import UIKit
+import MapKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var directionLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    let circuitDataManager = CircuitDataManager()
+    let locationManager = CLLocationManager()
+    var circuit: Circuit!
+    var id: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        checkLocationAuthorizationStatus()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.startUpdatingHeading()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        if let delegate =  UIApplication.shared.delegate as? AppDelegate{
+            if delegate.circuit != nil {
+                self.circuit = delegate.circuit
+            } else if id != nil {
+                circuitDataManager.readById(id: id, callback: { (circuit) in
+                    self.circuit = circuit
+                })
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func checkLocationAuthorizationStatus() {
+        if !(CLLocationManager.authorizationStatus() == .authorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        let angleDistance = newHeading.magneticHeading - (circuit.location?.magneticHeading)!
+        let metersDistance = Double((locationManager.location?.distance(from: (circuit.location?.toCLLocation())!))!)
+        self.distanceLabel.text = "You are \(metersDistance) meters from the circuit."
+        if angleDistance < -10 {
+            self.directionLabel.text = "Turn \((angleDistance+10) * (-1)) degrees to the right."
+        } else if angleDistance > 10 {
+            self.directionLabel.text = "Turn \((angleDistance-10)) degrees to the left."
+        } else {
+            
+                if metersDistance < Double((locationManager.location?.horizontalAccuracy)!) + Double((circuit.location?.acuraccy)!) {
+                    self.performSegue(withIdentifier: "toCloudArCircuit", sender: nil)
+                }
+            }
+        
+    }
 
     /*
     // MARK: - Navigation
